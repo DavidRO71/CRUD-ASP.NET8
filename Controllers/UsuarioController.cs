9f579187fc8242f22872d5900fe8b5d9d364e48b
David <dramono@gmail.com>
2025-03-04 11:04:47 +0100

    añado el boton para volver en el formulario de creación y modificación del usuario

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -45,11 +45,20 @@ namespace CRUD_NET8.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]  //Es para que la información la coja del formulario
-        public async Task<IActionResult> Crear(UsuarioCreateDTO usuarioDTO)
+        public async Task<IActionResult> Crear(UsuarioCreateDTO usuarioDTO, string submitButton)
         {
+            Console.WriteLine("Boton: " + submitButton);
+
+            // Si el botón presionado es "Volver", no validamos ModelState, solo regresamos a la vista anterior
+            if (submitButton == "Volver")
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
             //Vamos a utilizar un DTO pero para nuestro ejemplo no vamos a guardar todos los datos del usuario
             //Solo vamos a guardar el nombre, el apellido y el tipo de usuario
             if (ModelState.IsValid) {
+                Console.WriteLine("OK -----");
                 var usuario = new Usuario(){
                     UsuLogin = usuarioDTO.UsuLogin,
                     UsuPwd = usuarioDTO.UsuPwd,
@@ -61,6 +70,8 @@ namespace CRUD_NET8.Controllers
                 await _myDBContext.SaveChangesAsync();
 
                 return RedirectToAction(nameof(Index));
+            } else {
+                Console.WriteLine("NO OK");
             }
 
             //Aqui recuperamos los registros de la tabla TipoUsuario para mostrarla en 1 Combo en la vista
@@ -137,5 +148,13 @@ namespace CRUD_NET8.Controllers
 
             return RedirectToAction("Index");
         }
+
+        //*******************************************************
+        //* V O L V E R   A   L A   P A G I N A   I N I C I A L
+        //*******************************************************
+        public IActionResult Volver(){
+            return View();
+        }
     }
+
 }
